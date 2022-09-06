cbac8b744ef577fb3aef4c22b01dadc126d227ad
Irving78rus <isakov911@mail.ru>
2022-09-06 15:16:19 +0300

    Updates

--- src/components/Preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import preloader from '../img/1494.gif'
-let Preloader = (props) => {
-    return (
-        <img src={preloader} alt="preloader" />
-    )
-}
-export default Preloader
